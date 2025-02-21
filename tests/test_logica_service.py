b578873d86408bad62ed8b4f87a917416d081302
Santiago Lasso <santiagoflassop@gmail.com>
2025-02-20 22:12:46 -0500

    HU010 Verificar que la lista de entrenamientos está ordenada: Corrección fechas set up _(Rojo)

--- tests/test_logica_service.py
+++ tests/test_logica_service.py
@@ -44,10 +44,10 @@ class LogicaServiceTestCase(unittest.TestCase):
 
         #Creacion Entrenamientos
 
-        entrenamiento1 = Entrenamiento(fecha=datetime.date(2024, 12, 25),cat_repeticiones=12,tiempo=time(hour=0, minute=10, second=2))
-        entrenamiento2 = Entrenamiento(fecha=datetime.date(2024, 12, 20),cat_repeticiones=8,tiempo=time(hour=0, minute=5, second=2))
-        entrenamiento3 = Entrenamiento(fecha=datetime.date(2024, 12, 21), cat_repeticiones=5,tiempo=time(hour=0, minute=8, second=0))
-        entrenamiento4 = Entrenamiento(fecha=datetime.date(2024, 12, 27), cat_repeticiones=15,tiempo=time(hour=0, minute=12, second=0))
+        entrenamiento1 = Entrenamiento(fecha=datetime(2024, 8, 25),cat_repeticiones=12,tiempo=time(hour=0, minute=10, second=2))
+        entrenamiento2 = Entrenamiento(fecha=datetime(2024, 11, 21),cat_repeticiones=8,tiempo=time(hour=0, minute=5, second=2))
+        entrenamiento3 = Entrenamiento(fecha=datetime(2024, 12, 21), cat_repeticiones=5,tiempo=time(hour=0, minute=8, second=0))
+        entrenamiento4 = Entrenamiento(fecha=datetime(2024, 10, 27), cat_repeticiones=15,tiempo=time(hour=0, minute=12, second=0))
         self.session.add(entrenamiento1)
         self.session.add(entrenamiento2)
         self.session.add(entrenamiento3)
