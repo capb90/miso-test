4dc3029f4d956d7b6c31ff3f3cf677eec9109fd1
capb90 <capb.dev@gmail.com>
2025-02-27 11:36:00 -0500

    Fix(declarative_base):Engine db test

--- src/modelo/declarative_base.py
+++ src/modelo/declarative_base.py
@@ -2,12 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-IS_TEST = False
-
-DB_URL = 'sqlite:///enForma.sqlite' if IS_TEST else 'sqlite:///enFormaTest.sqlite'
-
-engine = create_engine(DB_URL,echo=IS_TEST)
-Session = sessionmaker(bind=engine)
-
 Base = declarative_base()
-session = Session()
+
+def create_context(is_test: bool = False):
+    DB_URL = 'sqlite:///db_app.sqlite'
+    if is_test:
+        DB_URL = 'sqlite:///db_test.sqlite'
+    engine = create_engine(DB_URL, echo=is_test)
+    Session = sessionmaker(bind=engine)
+
+    base = Base
+    session = Session()
+
+    return engine,base,session
--- tests/test_logica_service.py
+++ tests/test_logica_service.py
@@ -2,52 +2,63 @@ import unittest
 from datetime import datetime, time
 
 from src.logica.logica_service import LogicaService
-from src.modelo.declarative_base import Session, session
+from src.modelo.declarative_base import create_context
 from src.modelo.persona import Persona
 from src.modelo.ejercicio import Ejercicio
 from src.modelo.entrenamiento import Entrenamiento
-import src.modelo.declarative_base
 
 
 class LogicaServiceTestCase(unittest.TestCase):
 
     def setUp(self):
-        src.modelo.declarative_base.IS_TEST = True
-        self.logica = LogicaService()
+        engine, base, session = create_context(True)
+        self.logica = LogicaService(engine, base, session)
 
-        #Creacion datos de prueba
-        self.session = Session()
+        # Creacion datos de prueba
+        self.session = session
 
-        #Crear personas de prueba
-        persona1 = Persona(nombre="John",apellidos="Doe",fecha_inicio=datetime.today(),talla=176,peso=75,edad=32,medida_brazo=15,medida_pecho=80,medida_cintura=60,medida_pierna=70,medida_abdomen=50)
-        persona2 = Persona(nombre="Angelica",apellidos="Mora",fecha_inicio=datetime.today(),talla=156,peso=50,edad=22,medida_brazo=10,medida_pecho=50,medida_cintura=60,medida_pierna=30,medida_abdomen=40)
-        persona3 = Persona(nombre="Juan",apellidos="Doe",fecha_inicio=datetime.today(),talla=190,peso=80,edad=40,medida_brazo=20,medida_pecho=90,medida_cintura=70,medida_pierna=40,medida_abdomen=60)
-        persona4 = Persona(nombre="Andres",apellidos="Lopez",fecha_inicio=datetime.today(),talla=160,peso=70,edad=28,medida_brazo=30,medida_pecho=80,medida_cintura=70,medida_pierna=30,medida_abdomen=70)
+        # Crear personas de prueba
+        persona1 = Persona(nombre="John", apellidos="Doe", fecha_inicio=datetime.today(), talla=176, peso=75, edad=32,
+                           medida_brazo=15, medida_pecho=80, medida_cintura=60, medida_pierna=70, medida_abdomen=50)
+        persona2 = Persona(nombre="Angelica", apellidos="Mora", fecha_inicio=datetime.today(), talla=156, peso=50,
+                           edad=22, medida_brazo=10, medida_pecho=50, medida_cintura=60, medida_pierna=30,
+                           medida_abdomen=40)
+        persona3 = Persona(nombre="Juan", apellidos="Doe", fecha_inicio=datetime.today(), talla=190, peso=80, edad=40,
+                           medida_brazo=20, medida_pecho=90, medida_cintura=70, medida_pierna=40, medida_abdomen=60)
+        persona4 = Persona(nombre="Andres", apellidos="Lopez", fecha_inicio=datetime.today(), talla=160, peso=70,
+                           edad=28, medida_brazo=30, medida_pecho=80, medida_cintura=70, medida_pierna=30,
+                           medida_abdomen=70)
 
         self.session.add(persona1)
         self.session.add(persona2)
         self.session.add(persona3)
         self.session.add(persona4)
 
-
-        #Creacion Ejercicios
-        ejercicio1 = Ejercicio(nombre="Press de banca",descripcion="Lorem ipsum",calorias=80,enlace_video="https://ejercicio_1.com")
-        ejercicio2 = Ejercicio(nombre="Sentadilla",descripcion="Lorem ipsum",calorias=160,enlace_video="https://ejercicio_2.com")
-        ejercicio3 = Ejercicio(nombre="Remo con barra T",descripcion="Lorem ipsum",calorias=110,enlace_video="https://ejercicio_3.com")
-        ejercicio4 = Ejercicio(nombre="Press Militar",descripcion="Lorem ipsum",calorias=90,enlace_video="https://ejercicio_4.com")
+        # Creacion Ejercicios
+        ejercicio1 = Ejercicio(nombre="Press de banca", descripcion="Lorem ipsum", calorias=80,
+                               enlace_video="https://ejercicio_1.com")
+        ejercicio2 = Ejercicio(nombre="Sentadilla", descripcion="Lorem ipsum", calorias=160,
+                               enlace_video="https://ejercicio_2.com")
+        ejercicio3 = Ejercicio(nombre="Remo con barra T", descripcion="Lorem ipsum", calorias=110,
+                               enlace_video="https://ejercicio_3.com")
+        ejercicio4 = Ejercicio(nombre="Press Militar", descripcion="Lorem ipsum", calorias=90,
+                               enlace_video="https://ejercicio_4.com")
 
         self.session.add(ejercicio1)
         self.session.add(ejercicio2)
         self.session.add(ejercicio3)
         self.session.add(ejercicio4)
 
+        # Creacion Entrenamientos
 
-        #Creacion Entrenamientos
-
-        entrenamiento1 = Entrenamiento(fecha=datetime(2024, 8, 25),cat_repeticiones=12,tiempo=time(hour=0, minute=10, second=2))
-        entrenamiento2 = Entrenamiento(fecha=datetime(2024, 11, 21),cat_repeticiones=8,tiempo=time(hour=0, minute=5, second=2))
-        entrenamiento3 = Entrenamiento(fecha=datetime(2024, 12, 21), cat_repeticiones=5,tiempo=time(hour=0, minute=8, second=0))
-        entrenamiento4 = Entrenamiento(fecha=datetime(2024, 10, 27), cat_repeticiones=15,tiempo=time(hour=0, minute=12, second=0))
+        entrenamiento1 = Entrenamiento(fecha=datetime(2024, 8, 25), cat_repeticiones=12,
+                                       tiempo=time(hour=0, minute=10, second=2))
+        entrenamiento2 = Entrenamiento(fecha=datetime(2024, 11, 21), cat_repeticiones=8,
+                                       tiempo=time(hour=0, minute=5, second=2))
+        entrenamiento3 = Entrenamiento(fecha=datetime(2024, 12, 21), cat_repeticiones=5,
+                                       tiempo=time(hour=0, minute=8, second=0))
+        entrenamiento4 = Entrenamiento(fecha=datetime(2024, 10, 27), cat_repeticiones=15,
+                                       tiempo=time(hour=0, minute=12, second=0))
         self.session.add(entrenamiento1)
         self.session.add(entrenamiento2)
         self.session.add(entrenamiento3)
@@ -65,10 +76,7 @@ class LogicaServiceTestCase(unittest.TestCase):
         persona4.entrenamientos = [entrenamiento4]
         ejercicio4.entrenamientos = [entrenamiento4]
 
-
         self.session.commit()
-
-
 
     def tearDown(self):
         self.session.query(Entrenamiento).delete()
@@ -82,23 +90,22 @@ class LogicaServiceTestCase(unittest.TestCase):
         personas = self.logica.dar_personas()
         self.assertIsInstance(personas, list)
 
-
     def test_dar_persona_listado_no_vacio(self):
         personas = self.logica.dar_personas()
-        self.assertNotEqual(len(personas), 0 )
+        self.assertNotEqual(len(personas), 0)
 
     def test_dar_personas_organizado(self):
         personas = self.logica.dar_personas()
-        self.assertEqual(personas,sorted(personas,key=lambda p: p.nombre))
-    
+        self.assertEqual(personas, sorted(personas, key=lambda p: p.nombre))
+
     def test_dar_entrenamientos_listado(self):
         entrenamientos = self.logica.dar_entrenamientos(1)
         self.assertIsInstance(entrenamientos, list)
-    
+
     def test_dar_entrenameintos_listado_no_vacio(self):
         entrenamientos = self.logica.dar_entrenamientos(1)
-        self.assertNotEqual(len(entrenamientos), 0 )
-    
+        self.assertNotEqual(len(entrenamientos), 0)
+
     def test_regresar_entrenamientos(self):
         entrenamientos = self.logica.dar_entrenamientos(1)
         entreno = entrenamientos[0]
@@ -108,11 +115,11 @@ class LogicaServiceTestCase(unittest.TestCase):
 
     def test_comprobar_asociacion_entrenamientos(self):
         entrenamientos = self.logica.dar_entrenamientos(1)
-        self.assertTrue(all(ent.persona==1 for ent in entrenamientos))
-    
+        self.assertTrue(all(ent.persona == 1 for ent in entrenamientos))
+
     def test_dar_entrenamientos_organizado(self):
         entrenamientos = self.logica.dar_entrenamientos(1)
-        self.assertEqual(entrenamientos,sorted(entrenamientos,key=lambda p: p.fecha))
+        self.assertEqual(entrenamientos, sorted(entrenamientos, key=lambda p: p.fecha))
 
     def test_crear_entrenamientos_sin_asignacion(self):
         with self.assertRaises(ValueError) as context:
@@ -123,10 +130,11 @@ class LogicaServiceTestCase(unittest.TestCase):
     def test_asignacion_entrenamientos(self):
         persona1 = self.session.query(Persona).get(1)
         ejercicio1 = self.session.query(Ejercicio).get(1)
-        self.logica.crear_entrenamiento(persona1, ejercicio1, datetime(2025, 2, 20), 12, time(hour=0, minute=10, second=2))
+        self.logica.crear_entrenamiento(persona1, ejercicio1, datetime(2025, 2, 20), 12,
+                                        time(hour=0, minute=10, second=2))
         entrenamientos_asignados = self.session.query(Entrenamiento).order_by(Entrenamiento.id.desc()).first()
-        self.assertEqual(entrenamientos_asignados.persona,1)
-        self.assertEqual(entrenamientos_asignados.ejercicio,1)
+        self.assertEqual(entrenamientos_asignados.persona, 1)
+        self.assertEqual(entrenamientos_asignados.ejercicio, 1)
 
     def test_validacion_formato_tiempo(self):
         persona1 = self.session.query(Persona).get(1)
@@ -155,15 +163,18 @@ class LogicaServiceTestCase(unittest.TestCase):
         self.assertEqual(response, "El link del video es obligatorio")
 
     def test_validar_crear_ejercicio_calorias(self):
-        response = self.logica.validar_crear_editar_ejercicio("Sentadilla", "Lorem ipsum", "https://ejercicio.com", None)
+        response = self.logica.validar_crear_editar_ejercicio("Sentadilla", "Lorem ipsum", "https://ejercicio.com",
+                                                              None)
         self.assertEqual(response, "La cantidad de calorias es obligatorio")
 
     def test_validar_crear_ejercicio_calorias_numerico(self):
-        response = self.logica.validar_crear_editar_ejercicio("Sentadilla", "Lorem ipsum", "https://ejercicio.com", "Lorem ipsum")
+        response = self.logica.validar_crear_editar_ejercicio("Sentadilla", "Lorem ipsum", "https://ejercicio.com",
+                                                              "Lorem ipsum")
         self.assertEqual(response, "La cantidad de calorias debe ser un valor numerico")
 
     def test_validar_crear_ejercicio_exitoso(self):
-        response = self.logica.validar_crear_editar_ejercicio("Sentadilla", "Lorem ipsum", "https://ejercicio.com", "12")
+        response = self.logica.validar_crear_editar_ejercicio("Sentadilla", "Lorem ipsum", "https://ejercicio.com",
+                                                              "12")
         self.assertEqual(response, "")
 
     def test_crear_ejercicio(self):
@@ -174,5 +185,5 @@ class LogicaServiceTestCase(unittest.TestCase):
 
     def test_crear_ejercicio_verificar_almacenamiento(self):
         self.logica.crear_ejercicio("Sentadilla_Test", "Lorem ipsum", "https://ejercicio.com", "160")
-        serch_ejercicio = self.session.query(Ejercicio).filter(Ejercicio.nombre=="Sentadilla_Test").first()
+        serch_ejercicio = self.session.query(Ejercicio).filter(Ejercicio.nombre == "Sentadilla_Test").first()
         self.assertNotEqual(serch_ejercicio, None)
